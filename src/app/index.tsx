fcc3faa212a5de47fc325193cdaad81e135f3d58
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-07-03 15:39:14 +0530

    Add Zustand state management and update dependencies in package files

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import useBearStore from "@/state";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const bears = useBearStore(({ bears }) => bears);
+  const { increasePopulation } = useBearStore();
   return (
     <View style={styles.container}>
+        <Text style={styles.heading}>Total sightings: {bears}</Text>
+        <Button title="Increase population" onPress={() => increasePopulation()} />
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
@@ -13,5 +18,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
 });
